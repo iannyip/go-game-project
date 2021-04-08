3ef832aa0f2489606cebce1620030755b498ab02
iannyip <ianyip@nullspace.com.sg>
2021-04-08 10:54:20 +0800

    changed background colour and set password field to password type

--- src/loginModule.js
+++ src/loginModule.js
@@ -1,34 +1,35 @@
-import './styles.scss';
-import axios from 'axios';
+import "./styles.scss";
+import axios from "axios";
 
 export function renderLoginElements(callbackFunction) {
   // Declare login elements
-  const loginContainer = document.createElement('div');
-  const loginCol = document.createElement('div');
-  const loginName = document.createElement('input');
-  const loginPassword = document.createElement('input');
-  const loginBtn = document.createElement('button');
+  const loginContainer = document.createElement("div");
+  const loginCol = document.createElement("div");
+  const loginName = document.createElement("input");
+  const loginPassword = document.createElement("input");
+  const loginBtn = document.createElement("button");
 
   // Set placeholders, text and classes
-  loginName.placeholder = 'Username';
-  loginPassword.placeholder = 'Password';
+  loginName.placeholder = "Username";
+  loginPassword.placeholder = "Password";
+  loginPassword.type = "password";
 
   // Set attributes, class, CALLBACK of login button
-  loginBtn.innerText = 'Login';
-  loginBtn.classList.add('btn', 'btn-primary');
-  loginBtn.setAttribute('type', 'submit');
-  loginBtn.addEventListener('click', callbackFunction);
+  loginBtn.innerText = "Login";
+  loginBtn.classList.add("btn", "btn-primary");
+  loginBtn.setAttribute("type", "submit");
+  loginBtn.addEventListener("click", callbackFunction);
 
   // Assign id, add class, and append
   [loginName, loginPassword, loginBtn].forEach((element) => {
     element.id = element.placeholder || element.innerText;
-    element.classList.add('form-control', 'my-4');
+    element.classList.add("form-control", "my-4");
     loginCol.appendChild(element);
   });
 
   // Modify main container
-  loginContainer.classList.add('row', 'justify-content-center', 'my-4');
-  loginCol.classList.add('col-md-6');
+  loginContainer.classList.add("row", "justify-content-center", "my-4");
+  loginCol.classList.add("col-md-6");
   loginContainer.appendChild(loginCol);
 
   return loginContainer;
@@ -39,7 +40,5 @@ export function authUserLogin(name, password) {
     name,
     password,
   };
-  return axios
-    .post('/login', userInfo)
-    .catch((error) => console.log(error));
+  return axios.post("/login", userInfo).catch((error) => console.log(error));
 }
