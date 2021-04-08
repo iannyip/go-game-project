462bcda711e3e6feacd227b61d3d489902a875bd
iannyip <ianyip@nullspace.com.sg>
2021-04-08 22:38:32 +0800

    added basic logo to login pg.

--- src/loginModule.js
+++ src/loginModule.js
@@ -4,15 +4,30 @@ import axios from "axios";
 export function renderLoginElements(callbackFunction) {
   // Declare login elements
   const loginContainer = document.createElement("div");
+  const logoElement = document.createElement("div");
   const loginCol = document.createElement("div");
   const loginName = document.createElement("input");
   const loginPassword = document.createElement("input");
   const loginBtn = document.createElement("button");
 
+  const blackPiece = document.createElement("div");
+  const whitePiece = document.createElement("div");
+
   // Set placeholders, text and classes
   loginName.placeholder = "Username";
   loginPassword.placeholder = "Password";
   loginPassword.type = "password";
+
+  // Define logo
+  logoElement.innerText = "GO";
+  logoElement.classList.add("login-logo");
+  blackPiece.innerText = "⚫";
+  blackPiece.classList.add("black-logo");
+  whitePiece.innerText = "⚪";
+  whitePiece.classList.add("white-logo");
+  logoElement.appendChild(blackPiece);
+  logoElement.appendChild(whitePiece);
+  loginCol.appendChild(logoElement);
 
   // Set attributes, class, CALLBACK of login button
   loginBtn.innerText = "Login";
